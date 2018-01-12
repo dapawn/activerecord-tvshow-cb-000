cc1b2d8a5314fb59d1927f6f77c7535d87aa983b
David Kuruvilla <dapawn@gmail.com>
2018-01-12 02:19:57 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,11 +2,11 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     Show.maximum(:rating)
   end
-  
+
   def self.most_popular_show
      Show.maximum(:rating).name
   end
-  
+
   def self.lowest_rating
 
   end
@@ -14,15 +14,15 @@ class Show < ActiveRecord::Base
   def self.least_popular_show
 
   end
-  
+
   def self.ratings_sum
 
   end
-  
+
   def self.popular_shows
 
   end
-  
+
   def self.shows_by_alphabetical_order
 
   end
